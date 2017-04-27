872b522d61fb498ce215707c1c4af1ce75cf799f
Jeffrey Han <itdelatrisu@gmail.com>
2017-04-27 11:38:57 -0400

    Try catching a socket error...

--- automation/SocketInterface.py
+++ automation/SocketInterface.py
@@ -32,7 +32,10 @@ class serversocket:
     def _accept(self):
         """ Listen for connections and pass handling to a new thread """
         while True:
-            (client, address) = self.sock.accept()
+            try:
+                (client, address) = self.sock.accept()
+            except:  # [Errno 9] Bad file descriptor
+                break
             thread = threading.Thread(target=self._handle_conn, args=(client, address))
             thread.daemon = True
             thread.start()
